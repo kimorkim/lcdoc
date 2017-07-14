74118d9ab664c86a4df73584450dcba0c8856272
jykim <kimorkim@gmail.com>
2017-07-14 19:07:32 +0900

    add license checker type

--- index2.js
+++ index2.js
@@ -14,53 +14,82 @@ program
     .action(function(argv) {
         let result = [];
         let workingPath = argv || process.cwd();
-        nlf.find({ directory: workingPath }, function(err, items = []) {
-            items.forEach((item) => {
-                let textList = [];
-                if (item.id) {
-                    textList.push(`Package: ${item.id}`);
-                }
-                const licenseSources = item.licenseSources;
-                if (licenseSources && licenseSources.license) {
-                    const package = licenseSources.package || {};
-                    const license = licenseSources.license;
-                    const pSource = package.sources || [];
-                    const lSource = license.sources || [];
 
-                    let licenseUrl = '';
-
-                    pSource.forEach((p) => {
-                        textList.push(`License: ${p.license}`);
-                        if (p.url) {
-                            textList.push(`License Url: ${p.url}`);
-                        }
-                    });
-
-                    lSource.forEach((l) => {
-                        if (l.filePath) {
-                            const parser = path.parse(l.filePath);
-                            textList.push(`License Source: ${parser.base}`);
-                        }
-                        if (l.text) {
-                            textList.push(`Source Text:\n\n${l.text}`);
-                        }
-                    });
-                }
-                result.push(textList.join('\n'));
-            });
-
-            const outputPath = program.out;
-            const outputText = result.join('\n\n-------------------------------------------------------------------------\n\n');
-            if (outputPath) {
-                fs.writeFile(outputPath, outputText, 'utf8', (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(`Successfully created file '${outputPath}'.`);
-                    }
-                });
+        checker.init({
+            start: workingPath,
+            customFormat: {
+                "name": "",
+                "version": "",
+                "description": "",
+                "licenses": "",
+                "licenseFile": "none",
+                "licenseText": "none",
+                "licenseModified": "no"
+            },
+        }, function(err, items = {}) {
+            if (err) {
+                console.log(err);
             } else {
-                console.log(outputText);
+                const itemKeys = Object.keys(items);
+                itemKeys.forEach((key) => {
+                    const item = items[key];
+                    const { licenses, licenseFile, licenseText, repository } = item;
+                    let textList = [];
+                    textList.push(`Package: ${key}`);
+
+                    licenses && textList.push(`License: ${licenses}`);
+                    repository && textList.push(`Repository Url: ${repository}`);
+
+                    if (licenseFile) {
+                        const parser = path.parse(licenseFile);
+                        textList.push(`License Source: ${parser.base}`);
+                    }
+
+                    // licenseText && textList.push(`Source Text:\n\n${licenseText}`);
+
+                    // console.log(item);
+
+                    // if (licenseSources && licenseSources.license) {
+                    //     const package = licenseSources.package || {};
+                    //     const license = licenseSources.license;
+                    //     const pSource = package.sources || [];
+                    //     const lSource = license.sources || [];
+
+                    //     let licenseUrl = '';
+
+                    //     pSource.forEach((p) => {
+                            
+                    //         if (p.url) {
+                    //             textList.push(`License Url: ${p.url}`);
+                    //         }
+                    //     });
+
+                    //     lSource.forEach((l) => {
+                    //         if (l.filePath) {
+                    //             const parser = path.parse(l.filePath);
+                    //             textList.push(`License Source: ${parser.base}`);
+                    //         }
+                    //         if (l.text) {
+                    //             textList.push(`Source Text:\n\n${l.text}`);
+                    //         }
+                    //     });
+                    // }
+                    result.push(textList.join('\n'));
+                });
+
+                const outputPath = program.out;
+                const outputText = result.join('\n\n-------------------------------------------------------------------------\n\n');
+                if (outputPath) {
+                    fs.writeFile(outputPath, outputText, 'utf8', (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(`Successfully created file '${outputPath}'.`);
+                        }
+                    });
+                } else {
+                    console.log(outputText);
+                }
             }
         });
     });
