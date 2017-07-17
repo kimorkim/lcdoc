4bd4f07a963977ff1dda8494a86fc6389e770584
jykim <kimorkim@gmail.com>
2017-07-17 13:51:39 +0900

    update lcdoc

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
+
 const program = require('commander');
 const nlf = require('nlf');
 const path = require('path');
 const fs = require('fs');
 
+function appender(xs) {
+    xs = xs || [];
+    return function(x) {
+        xs.push(x);
+        return xs;
+    }
+}
+
 program
     .name('lcdoc')
     .version('0.0.1')
-    .option('-o, --out <path>', 'write the data to a specific file.');
+    .option('-o, --out <path>', 'write the data to a specific file.')
+    .option('-i, --ignore <pattern>', 'write the path to ignore.', appender(), [])
+    .option('-d, --depth <number>', 'how deep to traverse packages where 0 is the current package.json only');
 
 program
     .command('make [path]')
     .action(function(argv) {
+        const ignorePaths = program.ignore;
         let result = [];
         let workingPath = argv || process.cwd();
-        nlf.find({ directory: workingPath }, function(err, items = []) {
+        const option = {
+            directory: workingPath,
+        };
+        let depth = program.depth;
+        if(depth) {
+            option.depth = Number(depth);
+        }
+        nlf.find(option, function(err, items = []) {
             items.forEach((item) => {
                 let textList = [];
                 if (item.id) {
@@ -37,6 +56,15 @@ program
                     });
 
                     lSource.forEach((l) => {
+                        if(ignorePaths && ignorePaths.some((i)=> {
+                            var a = path.resolve(workingPath, i);
+                            if(l.filePath.indexOf(a) > -1) {
+                                return true;
+                            }
+                        })) {
+                            return;
+                        }
+
                         if (l.filePath) {
                             const parser = path.parse(l.filePath);
                             textList.push(`License Source: ${parser.base}`);
